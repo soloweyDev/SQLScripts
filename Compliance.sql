b4cc428f48e2627972892d21ae0baa285711da2f
Ануфриев Дмитрий <da@en-gy.ru>
2020-11-03 15:42:51 +0300

    Update Compliance.sql

--- Compliance.sql
+++ Compliance.sql
@@ -43,7 +43,7 @@ INSERT INTO LoginLog (Login, Moment)
 		('Warren Mendez','2020-10-04 00:32:14'),
 		('Asher Willis','2020-10-17 12:59:41'),
 		('Adrian Morrow','2020-10-04 04:02:24'),
-		('Fletcher Ingram','2020-10-27 08:39:55'),
+		('Fletcher Ingram','2020-10-13 08:39:55'),
 		('Tanner Potter','2020-10-26 15:25:27'),
 		('Isaiah England','2020-10-25 17:57:17'),
 		('Stephen Harmon','2020-10-16 12:53:15'),
@@ -55,16 +55,16 @@ INSERT INTO LoginLog (Login, Moment)
 		('Dillon Rosales','2020-10-21 06:42:51'),
 		('Cade Tran','2020-10-27 07:48:06'),
 		('Bradley Blanchard','2020-10-02 11:52:29'),
-		('George Shannon','2020-10-21 23:54:10'),
+		('Leo Sawyer','2020-10-18 23:54:10'),
 		('Leonard Sheppard','2020-10-17 11:34:19'),
 		('Reed Vinson','2020-10-29 08:44:52'),
 		('Theodore Vaughn','2020-10-29 05:13:32'),
 		('Emery Nelson','2020-10-18 22:49:22'),
 		('Driscoll Johnston','2020-10-07 02:25:19'),
-		('Fletcher Ingram','2020-10-11 06:06:08'),
+		('Fletcher Ingram','2020-10-13 06:06:08'),
 		('Cedric Conley','2020-10-21 13:58:06'),
 		('Phillip Merritt','2020-10-20 03:09:27'),
-		('Fletcher Ingram','2020-10-20 05:09:17'),
+		('Fletcher Ingram','2020-10-13 05:09:17'),
 		('Christopher Collier','2020-10-20 17:42:44'),
 		('Garrison Bowen','2020-10-26 05:52:22'),
 		('Colorado Blanchard','2020-10-23 15:54:51'),
@@ -78,7 +78,7 @@ INSERT INTO LoginLog (Login, Moment)
 		('Erasmus Lucas','2020-10-15 19:30:18'),
 		('George Lowe','2020-10-24 18:07:50'),
 		('Brent Riddle','2020-10-05 23:55:30'),
-		('Gannon Peters','2020-10-20 13:03:29'),
+		('Leo Sawyer','2020-10-18 13:03:29'),
 		('Coby Guerrero','2020-10-31 23:43:26'),
 		('Steven Conner','2020-10-03 16:52:14'),
 		('Bradley Townsend','2020-10-22 22:27:52'),
@@ -92,13 +92,13 @@ INSERT INTO LoginLog (Login, Moment)
 		('Mark Decker','2020-10-26 08:02:42'),
 		('George Puckett','2020-10-13 03:14:28'),
 		('Jacob Beck','2020-10-10 06:33:30'),
-		('Fletcher Ingram','2020-10-10 15:12:19'),
+		('Fletcher Ingram','2020-10-13 15:12:19'),
 		('Chester Casey','2020-10-09 20:00:23'),
 		('Fritz Graves','2020-10-31 23:15:57'),
 		('Nicholas Price','2020-10-21 14:58:44'),
 		('Marsden Noble','2020-10-13 04:30:46'),
 		('Keaton Velasquez','2020-10-29 14:29:04'),
-		('Harlan Collins','2020-10-03 21:14:42'),
+		('Leo Sawyer','2020-10-18 21:14:42'),
 		('Ciaran Yang','2020-10-22 03:06:24'),
 		('Carl Coleman','2020-10-21 14:23:39'),
 		('Blake Carroll','2020-10-27 17:09:56'),
@@ -119,7 +119,7 @@ INSERT INTO LoginLog (Login, Moment)
 		('Hall Francis','2020-10-31 20:34:25'),
 		('Prescott Edwards','2020-10-31 22:53:00'),
 		('Palmer Hurley','2020-10-17 01:15:48'),
-		('Marsden Hurley','2020-10-14 18:53:00'),
+		('Leo Sawyer','2020-10-18 18:53:00'),
 		('Tyrone Pate','2020-10-01 13:46:14'),
 		('Caleb Webster','2020-10-19 19:55:12'),
 		('Amery Guerra','2020-10-24 06:22:11'),
@@ -130,9 +130,23 @@ INSERT INTO LoginLog (Login, Moment)
 		('Carter Pratt','2020-10-29 07:09:33');
 GO
 
+SELECT Login, Moment
+	INTO #TempMonth
+	FROM LoginLog
+	WHERE MONTH(Moment) = (MONTH(GETDATE()) - 1)
 
-SELECT Login, COUNT(Login) FROM LoginLog
-	GROUP BY Login
+SELECT Login, DAY(Moment) as [day]
+	INTO #TempDay
+	FROM #TempMonth
+	GROUP BY DAY(Moment), Login
 	HAVING COUNT(Login) >= 5
 
-DELETE LoginLog
+SELECT ll.Login, ll.Moment
+	INTO #TempMoment
+	FROM LoginLog as ll
+	JOIN #TempDay as t ON ll.Login = t.Login AND DAY(ll.Moment) = t.day
+
+SELECT t.Login, MAX(tt.Moment) MaxDate FROM #TempDay t
+	LEFT JOIN #TempMoment tt ON t.Login = tt.Login
+	GROUP BY t.Login
+GO
